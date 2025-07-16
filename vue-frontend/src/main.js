b70bf53e07fca454d758a37374725547b5b657ee
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2025-07-16 20:47:10 +0200

    fix typo

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -3,7 +3,22 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 
-const app = createApp(App);
+function mountVueApp() {
+  if (document.getElementById("app")) {
+    return;
+  }
 
-app.use(createPinia());
-app.mount("#app");
+  const appDiv = document.createElement("div");
+  appDiv.id = "app";
+  document.body.appendChild(appDiv);
+
+  const app = createApp(App);
+  app.use(createPinia());
+  app.mount("#app");
+}
+
+if (window.onUpdateHook) {
+  window.onUpdateHook.push(mountVueApp);
+} else {
+  mountVueApp();
+}
