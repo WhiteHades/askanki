0343d0a4a3ddb05f0557f8589d57a1fc3b3c368f
Efaz Mohammed <44260523+WhiteHades@users.noreply.github.com>
2025-07-19 00:22:41 +0200

    add temp dev support for auto reload

--- __init__.py
+++ __init__.py
@@ -8,8 +8,14 @@ mw.addonManager.setWebExports(__name__, r"vue-frontend/dist/.*")
 def inject_vue_ui(web_content: WebContent, context: Any):
     if not isinstance(context, mw.reviewer.__class__):
         return
+    
+    dev = True
 
-    js_path = f"/_addons/{addon_package}/vue-frontend/dist/assets/index.js"    
-    web_content.js.append(js_path)
+    if dev:
+        dev_script = '<script type="module" src="http://localhost:5173/src/main.js"></script>'
+        web_content.head += dev_script
+    else:
+        js_path = f"/_addons/{addon_package}/vue-frontend/dist/assets/index.js"
+        web_content.js.append(js_path)
 
 gui_hooks.webview_will_set_content.append(inject_vue_ui)
